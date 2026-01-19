1676d3b462ce08a939fc8f0e239121ae2c2de16c
Thomas Schoffelen <thomas@schof.co>
2026-01-19 20:45:42 +0000

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Embedded Sessions demo
+
+This is a very simple plain HTML demo for embedding Examplary sessions using the [Embedded Sessions API](https://developers.examplary.ai/embed-sessions).
+It shows how to create an embed session, load it in an iframe, and listen for events from the embed session using the `postMessage` API.
+
+- [🌍 Live demo](https://examplary-ai.github.io/embed-sessions-demo/)
+- [📘 Documentation](https://developers.examplary.ai/embed-sessions)
+- [💻 Source code](./index.html)
---
+++ demo-helpers.js
@@ -0,0 +1,42 @@
+// window.baseUrl = 'https://api.examplary.ai'; // TODO: change this
+window.baseUrl = "http://localhost:3001"; // TODO: change this
+
+// Create some vars we can use
+window.userId = "";
+window.embedUrl = "";
+
+// Make sure the API key is stored in localStorage so you only have to enter it once.
+const apiKeyInput = document.querySelector("#api-key-input");
+apiKeyInput.value = localStorage.getItem("examplaryApiKey") || "";
+window.apiKey = apiKeyInput.value;
+apiKeyInput.addEventListener("change", (el) => {
+  const apiKey = el.target.value;
+  localStorage.setItem("examplaryApiKey", apiKey);
+  window.apiKey = apiKey;
+});
+
+// Function to log messages to the developer console panel.
+const consoleOutput = document.querySelector("#console-output");
+window.logToConsole = function (message) {
+  consoleOutput.textContent += `\n${message}`;
+  consoleOutput.parentNode.scrollTop = consoleOutput.scrollHeight;
+};
+
+// Hook into the global fetch function to log API requests and responses.
+const originalFetch = window.fetch;
+window.fetch = async (input, init) => {
+  logToConsole(`\n\n→ ${init.method || "GET"} ${input.replace(baseUrl, "")}`);
+  if (init && init.body) {
+    logToConsole(JSON.stringify(JSON.parse(init.body), null, 2));
+  }
+
+  const response = await originalFetch(input, init);
+
+  const statusIcon = response.ok ? "✓" : "✗";
+  logToConsole(`\n← ${statusIcon} Response: ${response.status}`);
+  const clonedResponse = response.clone();
+  const responseBody = await clonedResponse.json();
+  logToConsole(JSON.stringify(responseBody, null, 2));
+
+  return response;
+};
